f5d2c6997434f7de0067dfd5220c26fef1089c51
Messai <messai@github.com>
2023-10-17 12:41:01 +0300

    10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Define update_topics function
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name
+    """
+    mongo_collection.update_one({"name": name}, {"$set": {"topics": [*topics]}})
