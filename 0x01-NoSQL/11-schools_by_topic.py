465e742725e05917f16fd188db240702c50f573b
Messai <messai@github.com>
2023-10-17 13:04:14 +0300

    11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Define schools_by_topic function
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns list of school having a specific topic
+    """
+    return [*mongo_collection.find({"topic": topic})]
