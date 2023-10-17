4381ce603e785b52202157a045caeff9482f4c77
Messai <messai@github.com>
2023-10-17 12:25:34 +0300

    9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -9,4 +9,4 @@ def insert_school(mongo_collection, **kwargs):
     Inserts a new document in a collection bases on kwargs
     """
     new = mongo_collection.insert_one(kwargs)
-    return new.get('_id')
+    return new.inserted_id
