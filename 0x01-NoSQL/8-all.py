ac5cd7130ccf53fad95f0006e1d50397cb12d0c5
Messai <messai@github.com>
2023-10-17 11:41:04 +0300

    8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+Defines list_all function
+"""
+import pymongo
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in the given collection mongo_collection
+    """
+    return mongo_collecitons.find()
