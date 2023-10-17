43268155013b0fff996d5b17a8340ffc3dff499c
Messai <messai@github.com>
2023-10-17 12:10:26 +0300

    8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -9,4 +9,4 @@ def list_all(mongo_collection):
     """
     Lists all documents in the given collection mongo_collection
     """
-    return [*mongo_colleciton.find()]
+    return [doc for doc in mongo_colleciton.find()]
