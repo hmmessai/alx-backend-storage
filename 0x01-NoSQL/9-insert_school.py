53e08a2facb1ca1b54ff594d82e9ad6cd301cb37
Messai <messai@github.com>
2023-10-17 12:22:12 +0300

    9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Define insert school function
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection bases on kwargs
+    """
+    new = mongo_collection.insert_one(kwargs)
+    return new.get('_id')
