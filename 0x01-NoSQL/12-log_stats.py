4941de183678078b91e4a5bcc90e11d7da47540d
Messai <messai@github.com>
2023-10-17 19:20:47 +0300

    12-2

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,18 +2,19 @@
 """
 Provides some stats about Nginx logs stored in MongoDB
 """
-import pymongo
-from pymongo import MongoClient
+if __name__ == '__main__':
+    import pymongo
+    from pymongo import MongoClient
 
-client = MongoClient("mongodb://127.0.0.1:27017")
-nginx_logs = client.logs.nginx
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    nginx_logs = client.logs.nginx
 
-print(nginx_logs.count_documents({}), 'logs')
-print('Methods:')
-print('    method GET:', nginx_logs.count_documents({'method': 'GET'}))
-print('    method GET:', nginx_logs.count_documents({'method': 'POST'}))
-print('    method GET:', nginx_logs.count_documents({'method': 'PUT'}))
-print('    method GET:', nginx_logs.count_documents({'method': 'PATCH'}))
-print('    method GET:', nginx_logs.count_documents({'method': 'DELETE'}))
-print(nginx_logs.count_documents({'method': 'GET',
-                                  'path': '/status'}), 'status check')
+    print(nginx_logs.count_documents({}), 'logs')
+    print('Methods:')
+    print('    method GET:', nginx_logs.count_documents({'method': 'GET'}))
+    print('    method GET:', nginx_logs.count_documents({'method': 'POST'}))
+    print('    method GET:', nginx_logs.count_documents({'method': 'PUT'}))
+    print('    method GET:', nginx_logs.count_documents({'method': 'PATCH'}))
+    print('    method GET:', nginx_logs.count_documents({'method': 'DELETE'}))
+    print(nginx_logs.count_documents({'method': 'GET',
+                                      'path': '/status'}), 'status check')
