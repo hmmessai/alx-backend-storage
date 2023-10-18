51174ff94eccbcfcbc3427fdbb6f05d4cd60f182
Messai <messai@github.com>
2023-10-18 19:50:47 +0300

    done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -35,7 +35,7 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
-def replay(method):
+def replay(method: Callable)-> None:
     r = redis.Redis()
     name = method.__qualname__
     count = r.get(name)
