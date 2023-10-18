60877832eca82ac0ed83647e11893f06d110d062
Messai <messai@github.com>
2023-10-18 11:23:37 +0300

    12 n 102

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -7,7 +7,7 @@ with the top 10 of the most present IPs in the collection
 if __name__ == '__main__':
     from pymongo import MongoClient
 
-    client = MongoClient("mongodb://127.0.0.1:27017")
+    client = MongoClient()
     nginx_logs = client.logs.nginx
 
     print(nginx_logs.count_documents({}), 'logs')
--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,7 +5,7 @@ Provides some stats about Nginx logs stored in MongoDB
 if __name__ == '__main__':
     from pymongo import MongoClient
 
-    client = MongoClient("mongodb://127.0.0.1:27017")
+    client = MongoClient()
     nginx_logs = client.logs.nginx
 
     print(nginx_logs.count_documents({}), 'logs')
